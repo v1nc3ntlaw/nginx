ba8207c93c7852b6e03d27c1ac0fe0f594867d60
v1nc3ntlaw <v1nc3ntlaw@gmail.com>
2013-02-21 15:59:55 +0800

    install libcurl4-openssl-dev instead libcurl4-gnutls-dev

--- recipes/passenger.rb
+++ recipes/passenger.rb
@@ -42,7 +42,7 @@ node.default["nginx"]["passenger"]["gem_binary"] = nil
 packages = value_for_platform( ["redhat", "centos", "scientific", "amazon", "oracle"] => {
                                  "default" => %w(ruby-devel curl-devel) },
                                ["ubuntu", "debian"] => {
-                                 "default" => %w(ruby-dev libcurl4-gnutls-dev) } )
+                                 "default" => %w(ruby-dev libcurl4-openssl-dev) } )
 
 packages.each do |devpkg|
   package devpkg
